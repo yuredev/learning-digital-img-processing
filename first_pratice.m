cb4ecf73edbfff8450c884ea28e704d0a38a354a
yuredev <yurematias@ufrn.edu.br>
2021-10-23 22:38:04 -0300

    first pratice added

---
+++ first_pratice.m
@@ -0,0 +1,24 @@
+close all
+clear all
+
+im = imread('./squares.png');
+imnova = im;
+
+for i = 1:size(imnova, 1)
+    for j = 1:size(imnova, 2)
+        % se for azul
+        if (imnova(i, j, 3) > 220 && imnova(i, j, 1) < 200 && imnova(i, j, 2) < 200)
+            % pintar de vermelho
+            imnova(i, j, 3) = 0;
+            imnova(i, j, 1) = 255;
+        end
+    end
+end
+
+figure(1);
+janela = imshow(im);
+figure(2);
+janela2 = imshow(imnova);
+
+waitfor(janela);
+waitfor(janela2);
